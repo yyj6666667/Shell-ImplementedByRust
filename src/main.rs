6a33109d00e8c7f1950a2c0b7d0e3c80de3b67be
Benjamin Franklin <yyj6666667@gmail.com>
2026-01-04 15:39:07 +0800

    <

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-     print!("$ ");
-     io::stdout().flush().unwrap();
-
-     //command not found 
-     let mut command = String::new();
-     io::stdin().read_line(&mut command).unwrap();
-     println!("{}: command not found", command.trim());
+    loop {
+      print!("$ ");
+      io::stdout().flush().unwrap();
+ 
+      //command not found 
+      let mut command = String::new();
+      io::stdin().read_line(&mut command).unwrap();
+      println!("{}: command not found", command.trim());
+    }
 }
